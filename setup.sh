0c03137b2ad4cee455e2b0f578e46ec1897b0448
Burhan Büyükarslan <a.burhanbuyukarslan@gmail.com>
2022-04-30 13:56:19 +0300

    changed setup.sh

--- setup.sh
+++ setup.sh
@@ -33,6 +33,5 @@ sudo usermod -aG docker $USER && newgrp docker
 minikube start
 minikube addons enable ingress
 
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-chmod 700 get_helm.sh
-./get_helm.sh
+# create project on kubernetes
+kubectl apply -f .
